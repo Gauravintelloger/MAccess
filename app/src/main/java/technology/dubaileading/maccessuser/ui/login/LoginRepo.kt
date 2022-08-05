8fa902ae7951673b2552d5cf4f96ee0d710fd23f
Jigin <67336544+vpjigin@users.noreply.github.com>
2022-08-05 23:27:04 +0530

    added viewmodel in login activity

--- app/src/main/java/technology/dubaileading/maccessuser/ui/login/LoginRepo.kt
+++ app/src/main/java/technology/dubaileading/maccessuser/ui/login/LoginRepo.kt
@@ -1,7 +1,32 @@
 package technology.dubaileading.maccessuser.ui.login
 
-import technology.dubaileading.maccessuser.RetrofitResponse
+import android.content.Context
 
-class LoginRepo(response : RetrofitResponse<String>) {
+import technology.dubaileading.maccessuser.rest.endpoints.EmployeeEndpoint
+import technology.dubaileading.maccessuser.rest.entity.LoginRequest
+import technology.dubaileading.maccessuser.rest.entity.LoginResponse
+import technology.dubaileading.maccessuser.rest.request.ServerRequestFactory
+import technology.dubaileading.maccessuser.rest.request.SuccessCallback
+
+class LoginRepo(var callback: LoginRepoCallback) {
+
+    fun login(context : Context,request : LoginRequest){
+        val requestFactory = ServerRequestFactory()
+        val call = requestFactory
+            .obtainEndpointProxy(EmployeeEndpoint::class.java)
+            .login(request)
+
+        val request = requestFactory.newHttpRequest<Any>(context)
+            .withEndpoint(call)
+            .withProgressDialogue()
+            .withSuccessAndFailureCallback(object : SuccessCallback<LoginResponse?> {
+                override fun onSuccess(user: LoginResponse?) {
+                    callback.loginResponse(user)
+                }
+            }) {
+                callback.loginFailure(it)
+            }.build()
+        request.executeAsync()
+    }
 
 }
